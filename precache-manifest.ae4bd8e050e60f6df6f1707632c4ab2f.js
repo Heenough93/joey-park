57f7aac53ab7089e0cafba856b167c47f5917fe1
wheelbottle <park.hee.byeong@google.com>
2023-02-18 22:56:25 +0900

    Updates

---
+++ precache-manifest.ae4bd8e050e60f6df6f1707632c4ab2f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49d4d1de07bee0ca5cc76e14f411bae9",
+    "url": "/joey-park/index.html"
+  },
+  {
+    "revision": "5090f57b2e8a7a7c6d18",
+    "url": "/joey-park/static/js/2.2d80a9a0.chunk.js"
+  },
+  {
+    "revision": "914a85462fc888825cb3a498c2b9869c",
+    "url": "/joey-park/static/js/2.2d80a9a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f58252eefe2f8dc325d",
+    "url": "/joey-park/static/js/main.c777657e.chunk.js"
+  },
+  {
+    "revision": "b45d57e3f53118164922",
+    "url": "/joey-park/static/js/runtime-main.145ccc73.js"
+  },
+  {
+    "revision": "1920d4bc1375f5740c11003221a9c390",
+    "url": "/joey-park/static/media/bg-hero.1920d4bc.jpg"
+  },
+  {
+    "revision": "571bb83bb19decde285b1cef8169a74c",
+    "url": "/joey-park/static/media/img-contact.571bb83b.jpg"
+  },
+  {
+    "revision": "37fef9b2f1b772b77093eb9b2d840e4f",
+    "url": "/joey-park/static/media/img-feature.37fef9b2.jpg"
+  },
+  {
+    "revision": "3a8b946f29c948219f49adc70565209e",
+    "url": "/joey-park/static/media/img-works-01.3a8b946f.jpg"
+  },
+  {
+    "revision": "9dc0c5e3e6c7088daaaca1bc1cfdf29a",
+    "url": "/joey-park/static/media/img-works-02.9dc0c5e3.jpg"
+  },
+  {
+    "revision": "d0777cdf521fc300eeee1bd0609bc8ef",
+    "url": "/joey-park/static/media/img-works-03.d0777cdf.jpg"
+  },
+  {
+    "revision": "d4167098605b1feb78ce25b64c0fefc8",
+    "url": "/joey-park/static/media/me.d4167098.jpg"
+  }
+]);
