77a13cafc9f1a7b5b7bad1104b70b0206673362b
hbpark <park.hee.byeong@gmail.com>
2023-12-22 11:14:51 +0900

    test page 개발 - User

--- src/components/test/User.tsx
+++ src/components/test/User.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Button, Grid } from '@mui/material';
+import React, { ChangeEvent } from 'react';
+import { Button, Divider, Grid } from '@mui/material';
 
 
 const User = () => {
@@ -14,6 +14,35 @@ const User = () => {
   const [users, setUsers] = React.useState<any[]>([]);
   const [selectedUser, setSelectedUser] = React.useState<any | null>(null);
 
+  React.useEffect(() => {
+    setName(selectedUser ? selectedUser.name : '');
+    setEmail(selectedUser ? selectedUser.email : '');
+    setPassword(selectedUser ? selectedUser.password : '');
+  }, [selectedUser])
+
+  const [name, setName] = React.useState<string>('');
+  const [email, setEmail] = React.useState<string>('');
+  const [password, setPassword] = React.useState<string>('');
+
+  const handleChangeName = React.useCallback((event: ChangeEvent<HTMLInputElement>) => {
+    setName(event.target.value);
+  }, [])
+
+  const handleChangeEmail = React.useCallback((event: ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value);
+  }, [])
+
+  const handleChangePassword = React.useCallback((event: ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  }, [])
+
+  const handleClickReset = React.useCallback(() => {
+    setUsers([]);
+    setSelectedUser(null);
+    setName('');
+    setEmail('');
+    setPassword('');
+  }, [])
 
   const handleClickUserList = React.useCallback(async () => {
     await fetch(process.env.REACT_APP_BASE_URL + 'users', {
@@ -47,96 +76,97 @@ const User = () => {
     await fetch(process.env.REACT_APP_BASE_URL + 'users', {
       method: "POST",
       headers: { "Content-Type": "application/json", "Authorization": accessToken },
-      body: JSON.stringify({
-        "name": "user11",
-        "password": "qweqwe",
-        "email": "user11@test.com"
-      })
+      body: JSON.stringify({ name, password, email })
     })
       .then((res) => res.json())
       .then((res) => {
-        console.log(res.message);
-        console.log('User Register', res.data);
+        console.log({ res });
+        setSelectedUser(null);
+      })
+      .then(async () => {
+        await handleClickUserList();
       });
-  }, [accessToken])
+  }, [accessToken, name, password, email])
 
-  const handleClickUserModify = React.useCallback(async (id: string) => {
-    if (!id) {
-      alert('id is empty.');
+  const handleClickUserModify = React.useCallback(async () => {
+    if (!selectedUser) {
+      alert('You should select a user first.');
       return;
     }
-    await fetch(process.env.REACT_APP_BASE_URL + 'users/' + id, {
+    await fetch(process.env.REACT_APP_BASE_URL + 'users/' + selectedUser.id, {
       method: "PUT",
       headers: { "Content-Type": "application/json", "Authorization": accessToken },
-      body: JSON.stringify({
-        "name": "user12",
-        "password": "qweqwe",
-        "email": "user12@test.com"
-      })
+      body: JSON.stringify({ name, password, email })
     })
       .then((res) => res.json())
       .then((res) => {
-        console.log(res.message);
-        console.log('User Modify', res.data);
+        console.log({ res });
+        setSelectedUser(null);
+      })
+      .then(async () => {
+        await handleClickUserList();
       });
-  }, [accessToken])
+  }, [selectedUser, accessToken, name, password, email])
 
-  const handleClickUserRemove = React.useCallback(async (id: string) => {
-    if (!id) {
-      alert('id is empty.');
+  const handleClickUserRemove = React.useCallback(async () => {
+    if (!selectedUser) {
+      alert('You should select a user first.');
       return;
     }
-    await fetch(process.env.REACT_APP_BASE_URL + 'users/' + id, {
+    await fetch(process.env.REACT_APP_BASE_URL + 'users/' + selectedUser.id, {
       method: "DELETE",
       headers: { "Content-Type": "application/json", "Authorization": accessToken },
     })
       .then((res) => res.json())
       .then((res) => {
-        console.log(res.message);
-        console.log('User Remove', res.data);
+        console.log({ res });
+        setSelectedUser(null);
+      })
+      .then(async () => {
+        await handleClickUserList();
       });
-  }, [accessToken])
+  }, [selectedUser, accessToken])
 
   return (
     <div style={{ height: 600, width: '100%' }}>
       <div>
+        <Button onClick={handleClickReset}>Reset</Button>
         <Button onClick={handleClickUserList}>User List</Button>
-        {/*<Button onClick={handleClickUserRegister}>User Register</Button>*/}
-        {/*<Button onClick={() => handleClickUserModify(id)}>User Modify</Button>*/}
-        {/*<Button onClick={() => handleClickUserRemove(id)}>User Remove</Button>*/}
+        <Button onClick={handleClickUserRegister}>User Register</Button>
+        <Button onClick={handleClickUserModify}>User Modify</Button>
+        <Button onClick={handleClickUserRemove}>User Remove</Button>
       </div>
-
+      <Divider />
+      <div style={{ height: 200 }}>
+        {users.map((user, index) => {
+          return (
+            <div key={index} onClick={() => handleClickUser(user.id)}>
+              {`NAME: ${user.name} / EMAIL: ${user.email} / ROLE: ${user.role}`}
+            </div>
+          )
+        })}
+      </div>
+      <Divider />
       <Grid container spacing={2}>
         <Grid item xs={6} md={4}>
           NAME
         </Grid>
         <Grid item xs={6} md={8}>
-          {selectedUser.name}
-          {/*<input onChange={handleChangeName} value={name} />*/}
+          <input onChange={handleChangeName} value={name} />
         </Grid>
         <Grid item xs={6} md={4}>
           EMAIL
         </Grid>
         <Grid item xs={6} md={8}>
-          {selectedUser.email}
-          {/*<input onChange={handleChangeEmail} value={email} />*/}
+          <input onChange={handleChangeEmail} value={email} />
         </Grid>
         <Grid item xs={6} md={4}>
           PASSWORD
         </Grid>
         <Grid item xs={6} md={8}>
-          {selectedUser.password}
-          {/*<input onChange={handleChangePassword} value={password} />*/}
+          <input onChange={handleChangePassword} value={password} />
         </Grid>
       </Grid>
-
-      {users.map((user, index) => {
-        return (
-          <div key={index} onClick={() => handleClickUser(user.id)}>
-            {`NAME: ${user.name} / EMAIL: ${user.email} / ROLE: ${user.role}`}
-          </div>
-        )
-      })}
     </div>
   );
 }
