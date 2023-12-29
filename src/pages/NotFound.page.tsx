38be2ef5893328d001c95453035f67dc789582e8
hbpark <park.hee.byeong@gmail.com>
2023-12-29 15:03:11 +0900

    NotFound page 개발

---
+++ src/pages/NotFound.page.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+const NotFoundPage = () => {
+    //
+    return (
+      <>
+        <div id="wrapper">
+          <nav id="nav">
+            <span>
+              Not Found
+            </span>
+          </nav>
+        </div>
+      </>
+    );
+}
+
+export default NotFoundPage;
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,5 +1,6 @@
 export { default as MapPage } from './Map.page'
 export { default as MessagePage } from './Message.page'
+export { default as NotFoundPage } from './NotFound.page'
 export { default as PortfolioPage } from './Portfolio.page'
 export { default as StockPage } from './Stock.page'
 export { default as TestPage } from './Test.page'
